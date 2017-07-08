5d1cc00a0a15bd7b61a3c7f92042528af9d38b7c
Dave Greene <davepgreene@gmail.com>
2017-07-08 11:28:15 -0400

    Cleaned up GET function.

--- http/metadata.go
+++ http/metadata.go
@@ -1,12 +1,12 @@
 package http
 
 import (
+	"encoding/json"
 	"github.com/gorilla/handlers"
 	"net/http"
-	"encoding/json"
 
-	"github.com/davepgreene/propsd/sources"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/davepgreene/propsd/sources"
 )
 
 type metadataHandler struct{}
@@ -23,7 +23,6 @@ func (h *metadataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		m := sources.NewMetadataSource(*s)
 		m.Get()
-		m.Poll()
 
 		w.WriteHeader(http.StatusNotImplemented)
 		b, _ := json.Marshal(m.Properties())
@@ -31,4 +30,3 @@ func (h *metadataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Write(b)
 	}
 }
-
